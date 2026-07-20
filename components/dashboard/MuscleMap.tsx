3274987ace0d637c5d21877d8f3d47787321bf9d
Ari-rizz <156146503+Ari-rizz@users.noreply.github.com>
2026-07-20 23:42:50 +0200

    Updated MuscleMap.tsx

--- components/dashboard/MuscleMap.tsx
+++ components/dashboard/MuscleMap.tsx
@@ -218,7 +218,7 @@ export function MuscleMap({ trainedMuscles }: Props) {
           </span>
         </div>
       )}
-      <div className="flex justify-center items-start w-full gap-9">
+      <div className="flex justify-center items-start w-full gap-12">
         <div className="flex flex-col items-center gap-1">
           <div style={{ width: 110, height: 290 }}>
             <BodyView
